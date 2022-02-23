4fb5243e7548f1539d94a1ab7555af47ab18d837
Sawal Timsina <t.sawal231@gmail.com>
2022-02-23 17:07:49 +0545

    feat: Json response converter

---
+++ lib/converters/json_converter.dart
@@ -0,0 +1,46 @@
+import 'converter.dart';
+import 'converter_not_found.dart';
+
+typedef JsonFactory<T> = T Function(Map<String, dynamic> json);
+
+typedef ListFactory<T> = T Function(List<dynamic> list);
+
+class JsonResponseConverter implements ResponseConverter {
+  final Map<Type, JsonFactory> _jsonFactories;
+  final Map<Type, ListFactory> _listFactories;
+
+  JsonResponseConverter(this._jsonFactories, this._listFactories);
+
+  R _decodeMap<R>(Map<String, dynamic> values) {
+    final jsonFactory = _jsonFactories[R];
+    if (jsonFactory == null) {
+      throw ConverterNotFountException<R>();
+    }
+    return jsonFactory(values);
+  }
+
+  R _decodeList<R>(List values) {
+    final jsonFactory = _listFactories[R];
+    if (jsonFactory == null) {
+      throw ConverterNotFountException<R>();
+    }
+    return jsonFactory(values);
+  }
+
+  dynamic _decode<R>(entity) {
+    if (entity is Iterable) {
+      return _decodeList<R>(entity as List<dynamic>);
+    }
+
+    if (entity is Map) {
+      return _decodeMap<R>(entity as Map<String, dynamic>);
+    }
+
+    return entity;
+  }
+
+  @override
+  dynamic convert<ResultType>(dynamic data) {
+    return _decode<ResultType>(data);
+  }
+}
