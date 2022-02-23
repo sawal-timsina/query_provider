640337fd49ea7dc5631d4534f8943062e728f12f
Sawal Timsina <t.sawal231@gmail.com>
2022-02-23 17:30:16 +0545

    feat: query provider

---
+++ lib/providers/query_provider.dart
@@ -0,0 +1,25 @@
+import '../behaviours/query_behaviour.dart';
+import '../models/params.dart';
+import '../models/query_context.dart';
+import 'base_query_provider.dart';
+
+class QueryProvider<T extends dynamic> extends BaseQueryProvider<T> {
+  QueryProvider(
+    String query,
+    Future<dynamic> Function({QueryContext context}) queryFn, {
+    Params? params,
+    bool fetchOnMount = true,
+    void Function(T data)? onSuccess,
+    void Function(Exception error)? onError,
+    dynamic Function(Map<String, dynamic>)? select,
+  }) : super(
+          QueryBehaviour<T>(),
+          query,
+          queryFn,
+          params: params,
+          fetchOnMount: fetchOnMount,
+          onSuccess: onSuccess,
+          onError: onError,
+          select: select,
+        );
+}
