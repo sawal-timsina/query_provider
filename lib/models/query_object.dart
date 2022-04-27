5384c819ee8848270913dec80df94258aba0d86c
Sawal Timsina <t.sawal231@gmail.com>
2022-04-27 15:52:06 +0545

    feat: query,mutation object

--- lib/models/query_object.dart
+++ lib/models/query_object.dart
@@ -1,7 +1,29 @@
-class QueryObject<T extends dynamic> {
+class BaseQueryObject<T extends dynamic> {
   T? data;
   bool isLoading;
+  bool isError;
+
+  BaseQueryObject({this.data, required this.isLoading, required this.isError});
+}
+
+class QueryObject<T extends dynamic> extends BaseQueryObject<T> {
   bool isFetching;
 
-  QueryObject({this.data, required this.isLoading, required this.isFetching});
+  QueryObject({
+    T? data,
+    required bool isLoading,
+    required bool isError,
+    required this.isFetching,
+  }) : super(isLoading: isLoading, data: data, isError: isError);
+}
+
+class MutationObject<T extends dynamic> extends BaseQueryObject<T> {
+  bool isSuccess;
+
+  MutationObject({
+    T? data,
+    required bool isLoading,
+    required bool isError,
+    required this.isSuccess,
+  }) : super(isLoading: isLoading, data: data, isError: isError);
 }
