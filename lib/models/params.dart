af23ed5a5c545a3323abbd11c76672efa6ee1110
Sawal Timsina <t.sawal231@gmail.com>
2022-05-25 13:35:23 +0545

    feat: Params == operator

--- lib/models/params.dart
+++ lib/models/params.dart
@@ -2,4 +2,17 @@ abstract class Params {
   Map<String, dynamic> toJson();
 
   Params clone();
+
+  @override
+  bool operator ==(Object other) {
+    if (other is Params) {
+      final thisJson = toJson();
+      final oJson = other.toJson();
+      return thisJson.keys.every((key) => oJson[key] == thisJson[key]);
+    }
+    return false;
+  }
+
+  @override
+  int get hashCode => toJson().hashCode;
 }
